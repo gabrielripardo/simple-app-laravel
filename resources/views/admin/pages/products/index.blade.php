78e86728206306a1ecd68d1c36a8f5d18b4b0bd4
Ripardo <gts.senna@gmail.com>
2020-07-15 13:26:57 -0300

    Folders includes and componens and comp. created

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'Listagem de produtos')
     
 @section('content')
-    @include('admin/alerts/alerts', ['msg' => 'Mensagem de sucesso ou de erro.']) <!-- Alert com mensagem --> 
-    @include('admin/alerts/alerts') <!-- Alert sem mensagem --> 
+    @include('admin/includes/alerts/alerts', ['msg' => 'Mensagem de sucesso ou de erro.']) <!-- Alert com mensagem --> 
+    @include('admin/includes/alerts/alerts') <!-- Alert sem mensagem --> 
     <h1>Exibindo produtos...</h1>
     {{$name}}    
+
+    @component('admin/components/card')
+        @slot('title')
+            Titulo teste - card
+        @endslot
+            <h4>Conteúdo do Card Body</h4>
+    @endcomponent
+
 @endsection
 
