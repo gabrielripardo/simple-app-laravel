020a08e85001c602c73e7e0a51ccd80e5e90f77f
Ripardo <gts.senna@gmail.com>
2020-07-21 12:42:12 -0300

    Modal where product delete

--- resources/views/admin/pages/products/show.blade.php
+++ resources/views/admin/pages/products/show.blade.php
@@ -13,7 +13,30 @@
     <form action="{{ route('products.destroy', $product->id) }}" method="POST">
         @csrf
         @method('DELETE')
-        <button type="submit" class="btn btn-danger">Deletar</button>
-    </form>
 
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deletarModal">
+            Deletar
+        </button>          
+
+        <div class="modal fade" id="deletarModal" tabindex="-1" role="dialog" aria-labelledby="deletarModalLaber" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Deletar Produto</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Deseja deletar o produto?</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-primary">Sim</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+                </div>
+              </div>
+            </div>
+        </div>
+    </form>    
 @endsection
